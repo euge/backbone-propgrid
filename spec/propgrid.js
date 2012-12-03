d2a8a7b82912db84ce5a4f22e56f87c06cbceab9
Euge Gimelberg <eugene@rightscale.com>
2012-12-02 19:22:48 -0800

    Customizable input types

--- spec/propgrid.js
+++ spec/propgrid.js
@@ -1,46 +1,95 @@
-describe("PropertyGrid", function() {
-  
+describe("Propgrid", function() {
+
   var elTest, model;
-  
+
   beforeEach(function() {
     // create staging area for each test
     elTest = $("<div>")
     $("body").append(elTest);
-    
+
     model = new Backbone.Model({ Name : "Joe", Age : 30, State : "CA" });
   });
-    
-  it("should create an editor for all model attributes", function() {
-    var editor = new Propgrid({ model : model });
-    var elEditor = editor.render().$el;
-    var propNames = elEditor.find("tr .propgrid-attr").map(function() {
-      return $(this).text();
-    });
-    
-    expect(propNames.get().sort()).to.equalAsSets(["Name", "Age", "State"].sort());
-  });
-  
-  it("should create an editor for specific model attributes", function() {
-    var editor = new Propgrid({ model : model, attrs : [ "Name", "Age" ] });    
-    var elEditor = editor.render().$el;
-    var propNames = elEditor.find("tr .propgrid-attr").map(function() {
-      return $(this).text();
-    });
-    
-    expect(propNames.get().sort()).to.equalAsSets(["Name", "Age"].sort());
-  });
-  
-  it("should present an editable input when clicked on and revert back", function() {
-    var editor = new Propgrid({ model : model });
-    elTest.append(editor.render().$el);
-    
-    expect(elTest.find("tr .propgrid-value-edit:visible :input").length).to.equal(0);
-    elTest.find("tr .propgrid-value-show").first().click();
 
-    expect(elTest.find("tr .propgrid-value-edit:visible :input").length).to.equal(1);
-    
-    elTest.find("tr .propgrid-value-edit:visible :input").blur();
-    expect(elTest.find("tr .propgrid-value-edit:visible :input").length).to.equal(0);    
+  describe("Attributes", function() {
+
+    it("should create an editor for all model attributes", function() {
+      var editor = new Propgrid({ model : model });
+      var elEditor = editor.render().$el;
+      var propNames = elEditor.find("tr .propgrid-attr").map(function() {
+        return $(this).text();
+      });
+
+      expect(propNames.get().sort()).to.equalAsSets(["Name", "Age", "State"].sort());
+    });
+
+    it("should create an editor for specific model attributes", function() {
+      var editor = new Propgrid({ model : model, attrs : [ "Name", "Age" ] });
+      var elEditor = editor.render().$el;
+      var propNames = elEditor.find("tr .propgrid-attr").map(function() {
+        return $(this).text();
+      });
+
+      expect(propNames.get().sort()).to.equalAsSets(["Name", "Age"].sort());
+    });
+
   })
-    
+
+  describe("Events", function() {
+
+    it("should present an editable input when clicked on and revert back", function() {
+      var editor = new Propgrid({ model : model });
+      elTest.append(editor.render().$el);
+
+      expect(elTest.find("tr .propgrid-value-edit:visible :input").length).to.equal(0);
+      elTest.find("tr .propgrid-value-show").first().click();
+
+      expect(elTest.find("tr .propgrid-value-edit:visible :input").length).to.equal(1);
+
+      elTest.find("tr .propgrid-value-edit:visible :input").blur();
+      expect(elTest.find("tr .propgrid-value-edit:visible :input").length).to.equal(0);
+    });
+
+  });
+
+  describe("Types", function() {
+
+    describe("Text", function() {
+
+      it("should default to text inputs", function() {
+        var editor = new Propgrid({ model : model });
+        elTest.append(editor.render().$el);
+
+        expect(elTest.find(".propgrid-value-edit-text").length).to.equal(3);
+      });
+
+    });
+
+    describe("Select", function() {
+
+      it("should allow for the input type to be customized", function() {
+        var editor = new Propgrid({
+          model : model,
+          attrs : [ "State" ],
+          config : {
+            "State": { type : "Select", values : [ "CA", "MD", "WA" ] }
+          }
+        });
+
+        elTest.append(editor.render().$el);
+
+        expect(elTest.find(".propgrid-value-edit-select").length).to.equal(1);
+
+        elTest.find("tr .propgrid-value-show").first().click();
+        var opts = elTest.find(".propgrid-value-edit-select option").map(function() {
+          return $(this).val();
+        });
+
+        expect(opts.get()).to.equalAsSets([ "CA", "MD", "WA" ].sort());
+
+      });
+
+    });
+
+  });
+
 });
