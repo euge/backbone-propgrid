00a017ad8b26d7384b8c00f5e9af96ff840d04e1
Euge Gimelberg <eugene@rightscale.com>
2012-12-02 18:44:00 -0800

    More tests for edit mode

--- spec/propgrid.js
+++ spec/propgrid.js
@@ -1,17 +1,19 @@
-function getEl() {
-  var el = $("<div>")
-  $("body").append(el);
-  return el;
-}
-
 describe("PropertyGrid", function() {
+  
+  var elTest, model;
+  
+  beforeEach(function() {
+    // create staging area for each test
+    elTest = $("<div>")
+    $("body").append(elTest);
+    
+    model = new Backbone.Model({ Name : "Joe", Age : 30, State : "CA" });
+  });
     
   it("should create an editor for all model attributes", function() {
-    var model = new Backbone.Model({ Name : "Joe", Age : 30, State : "CA" });
     var editor = new Propgrid({ model : model });
-    
-    var el = editor.render().$el;
-    var propNames = el.find("tr .propgrid-attr-name").map(function() {
+    var elEditor = editor.render().$el;
+    var propNames = elEditor.find("tr .propgrid-attr").map(function() {
       return $(this).text();
     });
     
@@ -19,15 +21,26 @@ describe("PropertyGrid", function() {
   });
   
   it("should create an editor for specific model attributes", function() {
-    var model = new Backbone.Model({ Name : "Joe", Age : 30, State : "CA" });
-    var editor = new Propgrid({ model : model, attrs : [ "Name", "Age" ] });
-    
-    var el = editor.render().$el;
-    var propNames = el.find("tr .propgrid-attr-name").map(function() {
+    var editor = new Propgrid({ model : model, attrs : [ "Name", "Age" ] });    
+    var elEditor = editor.render().$el;
+    var propNames = elEditor.find("tr .propgrid-attr").map(function() {
       return $(this).text();
     });
     
     expect(propNames.get().sort()).to.equalAsSets(["Name", "Age"].sort());
   });
   
+  it("should present an editable input when clicked on and revert back", function() {
+    var editor = new Propgrid({ model : model });
+    elTest.append(editor.render().$el);
+    
+    expect(elTest.find("tr .propgrid-value-edit:visible :input").length).to.equal(0);
+    elTest.find("tr .propgrid-value-show").first().click();
+
+    expect(elTest.find("tr .propgrid-value-edit:visible :input").length).to.equal(1);
+    
+    elTest.find("tr .propgrid-value-edit:visible :input").blur();
+    expect(elTest.find("tr .propgrid-value-edit:visible :input").length).to.equal(0);    
+  })
+    
 });
